dd818f64c1c22b86ba67a1dcd0bb91f530adbb1e
Joy <94097424+jiyeon-520@users.noreply.github.com>
2024-07-25 19:46:09 +0900

    test commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello (): string {
+    console.log('kaka')
     return this.appService.getHello();
   }
 }
