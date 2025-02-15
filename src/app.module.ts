8f55b12dde996ad2b80440f46b3604ae7aec0b4c
Jay <Jay@geekstudio.co.kr>
2025-02-15 19:32:20 +0900

    :wrench: Fix: db 커넥션 정보 env로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,31 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PostModule } from './main/post.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'geek-custom-db-dev.c6nwyvdix4ge.ap-northeast-2.rds.amazonaws.com',
-      port: 3306,
-      username: 'geekstudio',
-      password: 'Geek2020!!',
-      database: 'geek-custom-db-dev',
-      autoLoadEntities: true,
-      synchronize: false,
-      charset: 'utf8mb4_unicode_ci',
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 사용 가능
+      envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, // 환경에 맞는 .env 파일 로드
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_DEFAULT_HOST'),
+        port: configService.get<number>('DB_DEFAULT_PORT'),
+        username: configService.get<string>('DB_DEFAULT_USER'),
+        password: configService.get<string>('DB_DEFAULT_PASSWORD'),
+        database: configService.get<string>('DB_DEFAULT_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: false,
+        charset: 'utf8mb4_unicode_ci',
+      }),
+    }),
+
     PostModule,
   ],
   controllers: [],
